17370c39a8662baa6c54689d4a2786323dac35ad
Aditya salunkhe <91340059+Aditya1404Sal@users.noreply.github.com>
2023-01-23 19:24:06 +0530

    Delete src directory

--- src/Timer.java
+++
@@ -1,6 +0,0 @@
-public class Timer{
-    public static double Starting_time = System.nanoTime();
-    public static double getTime(){
-        return (System.nanoTime() - Starting_time)*1E-9;
-    }
-}
